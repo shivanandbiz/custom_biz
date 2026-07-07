95a0be1c065c7dfc28138d46e91eb911fc1789f3
Shivu@2467 <shivanand.b@biztechnosys.com>
2026-07-07 11:37:00 +0530

    Fix Series query error

--- custom_biz/employee_naming.py
+++ custom_biz/employee_naming.py
@@ -25,7 +25,8 @@ def employee_autoname(doc, method):
             
             # Ensure the Series table is up to date
             series_name = doc.naming_series.split('.')[0] + '.'
-            current_series = frappe.db.get_value("Series", series_name, "current") or 0
+            current_series = frappe.db.sql("SELECT current FROM `tabSeries` WHERE name = %s", series_name)
+            current_series = current_series[0][0] if current_series else 0
             
             if current_series < num:
                 # Update series table to the highest existing number
