707c0babb8ded2eb4565e31c99488768e634436a
Shivu@2467 <shivanand.b@biztechnosys.com>
2026-08-20 15:07:11 +0530

    file click

--- custom_biz/public/js/custom_biz.js
+++ custom_biz/public/js/custom_biz.js
@@ -11,6 +11,38 @@ $(document).on("app_ready", function() {
 	}
 });
 
+// Handle folder click in Grid View to ensure clicking folder opens it instantly
+$(document).on("click", ".file-grid .file-wrapper", function(e) {
+	// Skip if clicking checkbox
+	if ($(e.target).is(":checkbox") || $(e.target).hasClass("list-row-checkbox")) {
+		return;
+	}
+
+	const $wrapper = $(this);
+	const name = $wrapper.attr("data-name");
+	
+	if (name) {
+		const doc_name = decodeURIComponent(name);
+
+		// If it has folder icon or type, navigate immediately
+		if ($wrapper.find(".icon-folder-normal-large, .icon-folder-normal, .folder-normal").length > 0) {
+			e.preventDefault();
+			e.stopPropagation();
+			frappe.set_route("List", "File", doc_name);
+			return;
+		}
+
+		// Fallback check via DB
+		frappe.db.get_value("File", doc_name, ["is_folder"]).then(r => {
+			if (r && r.message && r.message.is_folder) {
+				e.preventDefault();
+				e.stopPropagation();
+				frappe.set_route("List", "File", doc_name);
+			}
+		});
+	}
+});
+
 // Enforce grid_view = true whenever File Manager page loads
 frappe.router.on("change", function() {
 	const route = frappe.get_route();
