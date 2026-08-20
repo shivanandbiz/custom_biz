f2bb4e112224b2d12f200610ee533b0b54ac8a42
Shivu@2467 <shivanand.b@biztechnosys.com>
2026-08-20 16:01:27 +0530

    Sync Folder Equals filter pill on folder click and route change

--- custom_biz/public/js/custom_biz.js
+++ custom_biz/public/js/custom_biz.js
@@ -11,8 +11,20 @@ $(document).on("app_ready", function() {
 	}
 });
 
-// Handle folder click in Grid View to navigate directly into the folder
-$(document).on("click", ".file-grid .file-wrapper", function(e) {
+function apply_folder_filter_pill(folder_path) {
+	if (window.cur_list && window.cur_list.doctype === "File" && window.cur_list.filter_area) {
+		window.cur_list.filter_area.clear().then(() => {
+			window.cur_list.filter_area.add([["File", "folder", "=", folder_path, true]]);
+		}).catch(() => {
+			frappe.set_route("List", "File", folder_path);
+		});
+	} else {
+		frappe.set_route("List", "File", folder_path);
+	}
+}
+
+// Handle folder click in Grid View to apply the Folder Equals filter pill and open folder
+$(document).on("click", ".file-grid .file-wrapper, .file-list .list-row", function(e) {
 	// Skip if clicking checkbox
 	if ($(e.target).is(":checkbox") || $(e.target).hasClass("list-row-checkbox")) {
 		return;
@@ -24,11 +36,11 @@ $(document).on("click", ".file-grid .file-wrapper", function(e) {
 	if (name) {
 		const doc_name = decodeURIComponent(name);
 
-		// If it has folder icon or class, navigate directly into folder
-		if ($wrapper.find(".icon-folder-normal-large, .icon-folder-normal, .folder-normal").length > 0) {
+		// If clicking a folder (has folder icon or class)
+		if ($wrapper.find(".icon-folder-normal-large, .icon-folder-normal, .folder-normal").length > 0 || $wrapper.hasClass("folder")) {
 			e.preventDefault();
 			e.stopPropagation();
-			frappe.set_route("List", "File", doc_name);
+			apply_folder_filter_pill(doc_name);
 			return;
 		}
 
@@ -37,18 +49,32 @@ $(document).on("click", ".file-grid .file-wrapper", function(e) {
 			if (r && r.message && r.message.is_folder) {
 				e.preventDefault();
 				e.stopPropagation();
-				frappe.set_route("List", "File", doc_name);
+				apply_folder_filter_pill(doc_name);
 			}
 		});
 	}
 });
 
-// Enforce grid_view = true whenever File Manager page loads
+// Sync filter pill whenever route changes or File Manager page loads
 frappe.router.on("change", function() {
 	const route = frappe.get_route();
 	if (route && route[0] === "List" && route[1] === "File") {
 		if (frappe.views.FileView && (typeof frappe.views.FileView.grid_view === "undefined" || frappe.views.FileView.grid_view === false)) {
 			frappe.views.FileView.grid_view = true;
 		}
+
+		// Sync current route folder to filter pill
+		const current_folder = route.slice(2).join("/") || "Home";
+		setTimeout(() => {
+			if (window.cur_list && window.cur_list.doctype === "File" && window.cur_list.filter_area) {
+				const current_filters = window.cur_list.filter_area.get();
+				const has_folder_filter = current_filters.some(f => f[1] === "folder" && f[3] === current_folder);
+				if (!has_folder_filter) {
+					window.cur_list.filter_area.clear().then(() => {
+						window.cur_list.filter_area.add([["File", "folder", "=", current_folder, true]]);
+					});
+				}
+			}
+		}, 300);
 	}
 });
