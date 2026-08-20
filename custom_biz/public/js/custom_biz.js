cbd794c42013752f805cfd8b3ee4c707ea7bc0d4
Shivu@2467 <shivanand.b@biztechnosys.com>
2026-08-20 15:30:31 +0530

    Fix folder click navigation in Grid View

--- custom_biz/public/js/custom_biz.js
+++ custom_biz/public/js/custom_biz.js
@@ -11,21 +11,7 @@ $(document).on("app_ready", function() {
 	}
 });
 
-function apply_folder_filter(folder_path) {
-	if (window.cur_list && window.cur_list.doctype === "File" && window.cur_list.filter_area) {
-		try {
-			window.cur_list.filter_area.clear();
-		} catch (err) {}
-
-		setTimeout(() => {
-			window.cur_list.filter_area.add([["File", "folder", "=", folder_path, true]]);
-		}, 50);
-	} else {
-		frappe.set_route("List", "File", folder_path);
-	}
-}
-
-// Handle folder click in Grid View to apply the "Folder Equals [Folder Path]" filter
+// Handle folder click in Grid View to navigate directly into the folder
 $(document).on("click", ".file-grid .file-wrapper", function(e) {
 	// Skip if clicking checkbox
 	if ($(e.target).is(":checkbox") || $(e.target).hasClass("list-row-checkbox")) {
@@ -38,11 +24,11 @@ $(document).on("click", ".file-grid .file-wrapper", function(e) {
 	if (name) {
 		const doc_name = decodeURIComponent(name);
 
-		// If it has folder icon or class, apply folder filter immediately
+		// If it has folder icon or class, navigate directly into folder
 		if ($wrapper.find(".icon-folder-normal-large, .icon-folder-normal, .folder-normal").length > 0) {
 			e.preventDefault();
 			e.stopPropagation();
-			apply_folder_filter(doc_name);
+			frappe.set_route("List", "File", doc_name);
 			return;
 		}
 
@@ -51,7 +37,7 @@ $(document).on("click", ".file-grid .file-wrapper", function(e) {
 			if (r && r.message && r.message.is_folder) {
 				e.preventDefault();
 				e.stopPropagation();
-				apply_folder_filter(doc_name);
+				frappe.set_route("List", "File", doc_name);
 			}
 		});
 	}
